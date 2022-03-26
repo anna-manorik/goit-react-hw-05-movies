3f74170975964f481bfd9679264da504bf7099f1
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-26 19:58:18 +0200

    Create FilmList.js

---
+++ src/components/FilmList/FilmList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useMatch } from 'react-router-dom';
+
+export default function FilmList () {
+    const [films, setFilms] = useState([]);
+
+    async function fetchAPI() {
+      const URL =
+        'https://api.themoviedb.org/3/trending/all/day?api_key=893305900bfa999f02bdcec91556216a';
+  
+      const filmsApi = await fetch(URL)
+        .then(responce => responce.json())
+        .then(films => {
+          setFilms([...films.results]);
+        });
+    }
+
+    useEffect(() => {
+        fetchAPI();
+    }, [])
+
+  return (
+    <>
+    <p>Trending Films</p>
+    <ul>
+      {films.map(({ id, title }) => (
+        title && <li key={id}><Link to={`/movies/${id}`}>{title}</Link></li>
+        // <FilmListItem title={title} />
+      ))}
+    </ul>
+  </>
+  )
+};
+
