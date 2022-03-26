f29e2a4bab0e89700d0100197060aff715250b52
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-26 19:57:38 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,48 @@
-export const App = () => {
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Menu from './Menu/Menu';
+import FilmList from './FilmList/FilmList';
+import FilmSearch from './FilmSearch/FilmSearch';
+import FilmInfo from './FilmInfo/FilmInfo';
+import FilmCast from './FilmCast/FilmCast';
+import FilmReviews from './FilmReviews/FilmReviews';
+
+export default function App() {
+  const location = useLocation();
+
+  console.log(location);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<FilmList />} />
+
+        <Route path="/movies" element={<FilmSearch />} />
+
+        <Route path="/movies/:filmId" element={<FilmInfo />} />
+
+        <Route
+          path="/movies/:filmId/cast"
+          element={
+            <>
+              <FilmInfo />
+              <FilmCast />
+            </>
+          }
+        />
+
+        <Route
+          path="/movies/:filmId/reviews"
+          element={
+            <>
+              <FilmInfo />
+              <FilmReviews />
+            </>
+          }
+        />
+      </Routes>
+    </>
   );
-};
+}
