a4d2de3f5d69d3f76de3fd062965fee456f98fab
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-28 21:38:21 +0300

    Update FilmReviews.js

--- src/components/FilmReviews/FilmReviews.js
+++ src/components/FilmReviews/FilmReviews.js
@@ -1,5 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function FilmReviews () {
-  return <p>'REVIEWS'</p>;
+export default function FilmReviews() {
+  const { filmId } = useParams('');
+  const [reviewsList, setReviewsList] = useState([]);
+
+  async function fetchAPI() {
+    const URL = `https://api.themoviedb.org/3/movie/${filmId}/reviews?api_key=893305900bfa999f02bdcec91556216a`;
+
+    const filmsApi = await fetch(URL)
+      .then(responce => responce.json())
+      .then(reviews => {
+        setReviewsList([...reviews.results]);
+      });
+  }
+
+  useEffect(() => {
+    fetchAPI();
+  }, [filmId]);
+
+  return (
+    <>
+      <p>'REVIEWS'</p>
+      <ul>
+        {reviewsList.length > 0
+          ? reviewsList.map(review => (
+              <li>
+                <p>
+                  <b>Autor: {review.author}</b>
+                </p>
+                <p>{review.content}</p>
+              </li>
+            ))
+          : 'This fiml have not any reviews'}
+      </ul>
+    </>
+  );
 }
