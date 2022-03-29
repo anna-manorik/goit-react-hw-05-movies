a323a4d004d39a7a9fb6335ef24e5e0862b526d0
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-29 20:51:48 +0300

    Update FilmInfo.js

--- src/components/FilmInfo/FilmInfo.js
+++ src/components/FilmInfo/FilmInfo.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
+import PageNotFound from '../PageNotFound/PageNotFound';
 import styles from './styles.module.css';
 
 export default function FilmInfo() {
   const { filmId } = useParams('');
   const [film, setFilm] = useState({});
   const [genresList, setGenresList] = useState([]);
-
   const navigate = useNavigate();
+  const [isRender, setIsRender] = useState(true);
+
+  // let isRender = true;
 
   async function fetchAPI() {
     const URL = `https://api.themoviedb.org/3/movie/${filmId}?api_key=893305900bfa999f02bdcec91556216a`;
@@ -17,6 +20,10 @@ export default function FilmInfo() {
       .then(film => {
         setFilm(film);
         setGenresList(film.genres.map(genre => genre.name));
+      })
+      .catch(err => {
+        setIsRender(false);
+        console.log(isRender);
       });
   }
 
@@ -26,39 +33,45 @@ export default function FilmInfo() {
 
   return (
     <>
-      <button onClick={() => navigate(-1)}>Go back</button>
-      <div className={styles.container}>
-        <img
-          src={`https://image.tmdb.org/t/p/original/${film.poster_path}?api_key=893305900bfa999f02bdcec91556216a`}
-          alt={film.title}
-          width="300px"
-        />
-        <div className={styles.info}>
-          <h1>{film.title}</h1>
-          <p>
-            <b>Popularity:</b> {film.popularity}
-          </p>
-          <p>
-            <b>Overview:</b> {film.overview}
-          </p>
-          <p>
-            <b>Genres:</b> {genresList.join(', ')}
-          </p>
-        </div>
-      </div>
-      <hr />
+      {isRender ? (
+        <>
+          <button onClick={() => navigate(-1)}>Go back</button>
+          <div className={styles.container}>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${film.poster_path}?api_key=893305900bfa999f02bdcec91556216a`}
+              alt={film.title}
+              width="300px"
+            />
+            <div className={styles.info}>
+              <h1>{film.title}</h1>
+              <p>
+                <b>Popularity:</b> {film.popularity}
+              </p>
+              <p>
+                <b>Overview:</b> {film.overview}
+              </p>
+              <p>
+                <b>Genres:</b> {genresList.join(', ')}
+              </p>
+            </div>
+          </div>
+          <hr />
 
-      <div>
-        <ul>
-          <li>
-            <Link to={`/movies/${filmId}/cast`}>Cast</Link>
-          </li>
-          <li>
-            <Link to={`/movies/${filmId}/reviews`}>Reviews</Link>
-          </li>
-        </ul>
-      </div>
-      <hr />
+          <div>
+            <ul>
+              <li>
+                <Link to={`/movies/${filmId}/cast`}>Cast</Link>
+              </li>
+              <li>
+                <Link to={`/movies/${filmId}/reviews`}>Reviews</Link>
+              </li>
+            </ul>
+          </div>
+          <hr />
+        </>
+      ) : (
+        <PageNotFound />
+      )}
     </>
   );
 }
