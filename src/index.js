fc9e998995d3b835bd0145052dcda5e0fcf39b9d
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-29 17:13:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename='/goit-react-hw-05-movies/'>
+    <BrowserRouter basename='/goit-react-hw-05-movies'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
